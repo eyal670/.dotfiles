40d5c79f169f2edfdee1b447aab2e7b974bc2147
Eyal Ron <eyal670@gmail.com>
2022-03-31 10:45:45 +0300

    update fcd alias - fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,2 +1,3 @@
 alias cpwd="pwd | xclip -selection clipboard"
 alias fcd='cd (find $HOME/DevLab/ -type d | fzf)'
+alias fcd='cd (find $HOME/Docker/ $HOME/DevLab/ -type d 2>&1 | grep -v "Permission denied" | fzf)'
