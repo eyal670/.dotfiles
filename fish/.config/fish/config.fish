04bfb86d730946c304f2f2da92dcb615e6e19e09
eyal670 <eyal670@gmail.com>
2022-04-01 11:38:20 +0300

    add interactive statment - fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,3 +1,7 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+end
+
 alias cpwd="pwd | xclip -selection clipboard"
 alias fcd='cd (find $HOME/DevLab/ -type d | fzf)'
 alias fcd='cd (find $HOME/Docker/ $HOME/DevLab/ -type d 2>&1 | grep -v "Permission denied" | fzf)'
