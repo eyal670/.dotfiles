8f4a4ac87f652b33597e5c29c16b8506baa800ea
Eyal Ron <eyal670@gmail.com>
2022-04-07 01:14:14 +0300

    fish - set nvim as editor

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -4,4 +4,6 @@ end
 
 alias cpwd="pwd | xclip -selection clipboard"
 alias fcd='cd (find $HOME/DevLab/ -type d | fzf)'
-alias fcd='cd (find $HOME/Docker/ $HOME/DevLab/ -type d 2>&1 | grep -v "Permission denied" | fzf)'
+alias fcd='cd (find $HOME/DevLab/ -type d 2>&1 | grep -v "Permission denied" | fzf)'
+
+export EDITOR="nvim"
