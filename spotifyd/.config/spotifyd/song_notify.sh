982fe314d71315288bfe88a6a7b0372fe37f4685
eyal670 <eyal670@gmail.com>
2022-04-19 18:49:31 +0300

    spotifyd - song_notify set notify id to stop multiple notifications

--- spotifyd/.config/spotifyd/song_notify.sh
+++ spotifyd/.config/spotifyd/song_notify.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 song=$(playerctl metadata --player=spotifyd --format "Title: {{ xesam:title }}\nArtist: {{ xesam:artist }}\nAlbum: {{ xesam:album }}")
-notify-send "Music Player" "$song" --icon=~/.config/spotifyd/spotify.png
+dunstify "Music Player" "$song" --icon=~/.config/spotifyd/spotify.png -r 900 -u urgent
 
