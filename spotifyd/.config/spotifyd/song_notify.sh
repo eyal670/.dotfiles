03f18feb9a9f649a7526000744f1604fb49d5e68
eyal670 <eyal670@gmail.com>
2022-04-19 19:03:47 +0300

    spotifyd - song_notify change notification title

--- spotifyd/.config/spotifyd/song_notify.sh
+++ spotifyd/.config/spotifyd/song_notify.sh
@@ -1,4 +1,4 @@
 #!/bin/sh
 song=$(playerctl metadata --player=spotifyd --format "Title: {{ xesam:title }}\nArtist: {{ xesam:artist }}\nAlbum: {{ xesam:album }}")
-dunstify "Music Player" "$song" --icon=~/.config/spotifyd/spotify.png -r 900 -u urgent
+dunstify "Spotifyd" "$song" --icon=~/.config/spotifyd/spotify.png -r 900 -u urgent
 
