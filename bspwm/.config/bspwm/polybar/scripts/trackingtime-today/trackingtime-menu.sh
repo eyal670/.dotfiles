ecbafae5df317b4fc2b513ed1206b7da93f3aeee
eyal670 <eyal670@gmail.com>
2022-08-26 11:46:43 +0300

    trackingtime rofi position in top right corner

--- bspwm/.config/bspwm/polybar/scripts/trackingtime-today/trackingtime-menu.sh
+++ bspwm/.config/bspwm/polybar/scripts/trackingtime-today/trackingtime-menu.sh
@@ -3,7 +3,7 @@
 lines=4
 last_month_start=$(date --date="$(date +'%Y-%m-01') - 1 month" +%Y-%m-%d)
 last_month_end=$(date --date="$(date +'%Y-%m-01') - 1 second" +%Y-%m-%d)
-chosen=$(echo "this month|last month|today|set manually" | rofi -sep '|' -dmenu -p "TrackingTime total >" -i -l $lines )
+chosen=$(echo "this month|last month|today|set manually" | rofi -sep '|' -dmenu -p "TrackingTime total >" -i -l $lines -theme-str 'window { anchor:northeast;location:northeast; }' )
 case "$chosen" in
 	"today") $HOME/.config/bspwm/polybar/scripts/trackingtime-today/main.py ;;
 	"this month") $HOME/.config/bspwm/polybar/scripts/trackingtime-today/main.py -tf month ;;
@@ -13,7 +13,7 @@ case "$chosen" in
         -ed $last_month_end   ;;
 	"set manually") $HOME/.config/bspwm/polybar/scripts/trackingtime-today/main.py \
         -tf set \
-        -sd $(echo $last_month_start | rofi -sep '|' -dmenu -p "start date > " -mesg "format: YYYY-M-D" -l $lines) \
-        -ed $(echo $last_month_end | rofi -sep '|' -dmenu -p "end date > " -mesg "format: YYYY-M-D" -l $lines)   ;;
+        -sd $(echo $last_month_start | rofi -sep '|' -dmenu -p "start date > " -mesg "format: YYYY-M-D" -l $lines -theme-str 'window { anchor:northeast;location:northeast; }') \
+        -ed $(echo $last_month_end | rofi -sep '|' -dmenu -p "end date > " -mesg "format: YYYY-M-D" -l $lines -theme-str 'window { anchor:northeast;location:northeast; }')   ;;
 	*) exit 1 ;;
 esac
