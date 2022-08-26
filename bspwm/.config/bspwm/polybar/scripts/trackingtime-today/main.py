a310d1a789e5e9e2a94ee65b0814d0219bf3d31e
eyal670 <eyal670@gmail.com>
2022-08-26 03:35:02 +0300

    trackingtime - add option for current month,last,manual timeframe report

--- bspwm/.config/bspwm/polybar/scripts/trackingtime-today/main.py
+++ bspwm/.config/bspwm/polybar/scripts/trackingtime-today/main.py
@@ -15,14 +15,36 @@ import requests
 import json
 from math import modf
 from datetime import date
+import calendar
 import subprocess as subp
+import argparse
+
+# set cli args
+parser = argparse.ArgumentParser()
+
+parser.add_argument("-tf", "--timeframe", type=str, default="today", help="options: today, month, set")
+parser.add_argument("-sd", "--startdate", help="date object: 2022-08-01")
+parser.add_argument("-ed", "--enddate", help="date object: 2022-08-31")
+
+args = parser.parse_args()
 
 # loading .env variables
 load_dotenv()
 
 def get_entries(token,user_id,title):
-    today = str(date.today())
-    url = "https://app.trackingtime.co/api/v4/events?filter=USER&id="+user_id+"&from="+today+"&to="+today
+    today = date.today()
+    timeframe = args.timeframe 
+    if(timeframe == 'month'):
+        start_date = date(today.year,today.month,1)
+        lastday = calendar.monthrange(today.year, today.month)[1]
+        end_date = date(today.year,today.month,lastday)
+    elif (timeframe == 'set'):
+        start_date = args.startdate
+        end_date = args.enddate
+    else:
+        start_date = today
+        end_date = today
+    url = "https://app.trackingtime.co/api/v4/events?filter=USER&id="+user_id+"&from="+str(start_date)+"&to="+str(end_date)
     payload={}
     headers = {
       'Authorization': 'Basic '+token,
@@ -57,14 +79,18 @@ def format_hrs(hrs):
     return format_hrs
 
 def notify(data1,data2):
+    if(args.timeframe == 'set'):
+        timeframe = 'manually '+args.startdate+" - "+args.enddate
+    else:
+        timeframe = args.timeframe
     total_hrs = str(data1['raw_hrs'] + data2['raw_hrs'])
     data = "\nfor " + data1['title'] + "\ntotal hrs: " + data1['total_hrs'] + \
         "\ndecimal format: " + data1['decimal_hrs'] + \
         "\n\nfor " + data2['title'] + "\ntotal hrs: " + data2['total_hrs'] + \
         "\ndecimal format: " + data2['decimal_hrs'] + \
-        "\n\ntotal today " + "\ntotal hrs: " + format_hrs(total_hrs) + \
+        "\n\ntotal "+ timeframe + "\ntotal hrs: " + format_hrs(total_hrs) + \
         "\ndecimal format: " + str(round(float(total_hrs),2)) 
-    subp.run(["dunstify", "TrackingTime today", data, '-r', '901'])
+    subp.run(["dunstify", "TrackingTime "+timeframe, data, '-r', '901'])
 
 divine_data = get_entries(os.getenv('DIVINE_TOKEN'),os.getenv('DIVINE_USER'),'Divine')
 eyal_web_data = get_entries(os.getenv('EYALWEB_TOKEN'),os.getenv('EYALWEB_USER'),'Eyal Web')
