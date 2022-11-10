371a9f8d07661c2aad42e1c02e467927ac4ba813
eyal670 <eyal670@gmail.com>
2022-11-11 01:41:49 +0200

    tmux - cheat.sh update

--- bspwm/.config/bspwm/scripts/cheat.sh
+++ bspwm/.config/bspwm/scripts/cheat.sh
@@ -1,11 +1,27 @@
 #!/usr/bin/env bash
+
+#select from a list of all topics
 selected=`curl cht.sh/:list | fzf`
 if [[ -z $selected ]]; then
     exit 0
 fi
-clear
-read -p "Enter Query: " query
-read -p "Seperator(~,/)[/]: " seperator
-seperator=${seperator:-/}
+
+# check for sub topics and set matching seperator
+if [[ $(curl cht.sh/$selected/:list) = *[!\ ]* ]]; then
+    clear
+    echo topic: $selected
+    seperator=/ 
+    read -p "Enter sub topic, a question or leave empty: " query
+else
+    clear
+    echo topic: $selected
+    seperator=\~ 
+    read -p "Enter inner section or leave empty: " query
+fi
+
+# strip the seperator if query is empty
+if [ -z "${query}" ];then
+    seperator=''
+fi
 query=`echo $query | tr ' ' '+'`
 tmux neww bash -c "curl -s cht.sh/$selected$seperator$query | less -R"
