416c3ec761f208b7e1fdd40cc4ad88d57fd89f3c
eyal670 <eyal670@gmail.com>
2023-04-14 14:08:33 +0300

    auto install packer

--- nvim/.config/nvim/lua/er/plugins.lua
+++ nvim/.config/nvim/lua/er/plugins.lua
@@ -1,5 +1,14 @@
 -- This file can be loaded by calling `lua require('plugins')` from your init.vim
 
+-- Install packer
+local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nvim'
+local is_bootstrap = false
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+  is_bootstrap = true
+  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  vim.cmd [[packadd packer.nvim]]
+end
+
 -- Only required if you have packer configured as `opt`
 vim.cmd [[packadd packer.nvim]]
 
