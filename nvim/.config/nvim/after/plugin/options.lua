992dbf36f4ed2ea05dd562ebff43cbce18f02404
eyal670 <eyal670@gmail.com>
2023-04-18 22:54:09 +0300

    nvim - fix tab extra width

--- nvim/.config/nvim/after/plugin/options.lua
+++ nvim/.config/nvim/after/plugin/options.lua
@@ -4,7 +4,7 @@ vim.opt.relativenumber = true
 
 vim.opt.tabstop = 4
 vim.opt.softtabstop = 4
-vim.opt.shiftwidth = 4
+vim.opt.shiftwidth = 0
 vim.opt.expandtab = true
 
 vim.opt.smartindent = true
@@ -26,4 +26,3 @@ vim.opt.signcolumn = "yes"
 vim.opt.isfname:append("@-@")
 
 vim.opt.updatetime = 50
-
