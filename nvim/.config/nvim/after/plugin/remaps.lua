ac4b80b169213bbaf5c090a019971b7288c60697
eyal670 <eyal670@gmail.com>
2023-05-05 00:37:01 +0300

    nvim - add toggleterm

--- nvim/.config/nvim/after/plugin/remaps.lua
+++ nvim/.config/nvim/after/plugin/remaps.lua
@@ -35,4 +35,18 @@ vim.keymap.set('n', '<leader>Cf', "<cmd>:let @+ = expand('%:p') | !dunstify \"fu
   { desc = 'copy file path to clipboard' })
 
 -- escape terminal and go back to prev buffer
-vim.keymap.set('t', '<C-w>w', "<C-\\><C-N><C-w>w", { desc = 'back to prev buffer' })
+-- vim.keymap.set('t', '<C-w>w', "<C-\\><C-N><C-w>w", { desc = 'back to prev buffer' })
+
+function _G.set_terminal_keymaps()
+  local opts = { buffer = 0 }
+  vim.keymap.set('t', '<esc>', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
+  vim.keymap.set('t', '<C-h>', [[<Cmd>wincmd h<CR>]], opts)
+  vim.keymap.set('t', '<C-j>', [[<Cmd>wincmd j<CR>]], opts)
+  vim.keymap.set('t', '<C-k>', [[<Cmd>wincmd k<CR>]], opts)
+  vim.keymap.set('t', '<C-l>', [[<Cmd>wincmd l<CR>]], opts)
+  vim.keymap.set('t', '<C-w>', [[<C-\><C-n><C-w>]], opts)
+end
+
+-- if you only want these mappings for toggle term use term://*toggleterm#* instead
+vim.cmd('autocmd! TermOpen term://* lua set_terminal_keymaps()')
