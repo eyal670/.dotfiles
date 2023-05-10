f987413f46f23c9cb2c405ff28fe8305fdc1a4b4
eyal670 <eyal670@gmail.com>
2023-05-11 01:08:32 +0300

    nvim - custom command to append timestamp to current word

--- nvim/.config/nvim/after/plugin/custom-commands.lua
+++ nvim/.config/nvim/after/plugin/custom-commands.lua
@@ -1,5 +1,8 @@
-vim.cmd('command -nargs=0 Timestamp lua TimeStamp()')
+vim.api.nvim_create_user_command("TimestampCopy", function()
+  vim.fn.system("date +%s%3N | xclip -selection clipboard && notify-send \"timestamp copy to clipbaord\"")
+end, {})
 
-function TimeStamp()
-  vim.fn.system('date +%s%3N | xclip -selection clipboard && notify-send "timestamp copy to clipbaord"')
-end
+vim.api.nvim_create_user_command("TimestampAppend", function()
+  local timestamp = string.sub(vim.fn.system("date +%s%3N"), 1, -2)
+  vim.api.nvim_command("s:\\%#\\w\\+\\zs:_" .. timestamp .. ":")
+end, {})
