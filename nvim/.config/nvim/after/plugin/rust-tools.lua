e1fd1039b40c99357e43bb8bc959619dde276751
eyal670 <eyal670@gmail.com>
2023-05-23 04:50:15 +0300

    nvim - rust-tools rmaps

--- nvim/.config/nvim/after/plugin/rust-tools.lua
+++ nvim/.config/nvim/after/plugin/rust-tools.lua
@@ -3,10 +3,18 @@ local rt = require("rust-tools")
 rt.setup({
   server = {
     on_attach = function(_, bufnr)
-      -- Hover actions
-      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, { buffer = bufnr })
-      -- Code action groups
-      -- vim.keymap.set("n", "<Leader>a", rt.code_action_group.code_action_group, { buffer = bufnr })
+      vim.keymap.set("n", "<C-space>", rt.hover_actions.hover_actions, {
+        desc = "Hover actions",
+        buffer = bufnr
+      })
+      vim.keymap.set("n", "<Leader>ra", rt.code_action_group.code_action_group, {
+        desc = "Code action groups",
+        buffer = bufnr
+      })
+      vim.keymap.set("n", "<Leader>rr", "<cmd>:RustRun<cr>", {
+        desc = "Run Code",
+        buffer = bufnr
+      })
     end,
   },
 })
