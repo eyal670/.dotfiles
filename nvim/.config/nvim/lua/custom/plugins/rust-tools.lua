4c3aab29e5a1d18cfff7f07eb3d156c4dc012012
eyal670 <eyal670@gmail.com>
2023-05-23 03:58:03 +0300

    nvim - add rust-tools plugin

---
+++ nvim/.config/nvim/lua/custom/plugins/rust-tools.lua
@@ -0,0 +1,10 @@
+return {
+  'simrat39/rust-tools.nvim',
+  dependencies = {
+    -- LSP Support
+    { 'neovim/nvim-lspconfig' }, -- Required
+    { 'nvim-lua/plenary.nvim' }, -- Required
+    { 'mfussenegger/nvim-dap' }, -- Required
+  }
+
+}
