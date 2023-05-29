401f6a73650d7692bbd3d98ed9abf7854a283d0a
eyal670 <eyal670@gmail.com>
2023-05-30 01:15:32 +0300

    rust specific keymaps, run cargo clippy on save

--- nvim/.config/nvim/after/plugin/rust-tools.lua
+++ nvim/.config/nvim/after/plugin/rust-tools.lua
@@ -11,10 +11,20 @@ rt.setup({
         desc = "Code action groups",
         buffer = bufnr
       })
-      vim.keymap.set("n", "<Leader>rr", "<cmd>:RustRun<cr>", {
+      vim.keymap.set("n", "<Leader>rr", "<cmd>:TtoggleTerm cargo run<cr>", {
         desc = "Run Code",
         buffer = bufnr
       })
     end,
+    settings = {
+      -- to enable rust-analyzer settings visit:
+      -- https://github.com/rust-analyzer/rust-analyzer/blob/master/docs/user/generated_config.adoc
+      ["rust-analyzer"] = {
+        -- enable clippy on save
+        checkOnSave = {
+          command = "clippy",
+        },
+      },
+    },
   },
 })
