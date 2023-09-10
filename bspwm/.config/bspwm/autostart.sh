24025f4018b6e23484cc4e096478db3d3a050742
eyal670 <eyal670@gmail.com>
2023-09-10 10:31:33 +0300

    bspwm - autostart - screen lock setting notification

--- bspwm/.config/bspwm/autostart.sh
+++ bspwm/.config/bspwm/autostart.sh
@@ -44,9 +44,11 @@ unclutter -grab &
 
 #lock screen on idle timer
 #options: --not-when-audio, --not-when-fullscreen
+locktimer=900
+inminuts=$((locktimer/60))
 if [ ! $(pgrep "xidlehook") ]; then
-  notify-send "setting lock time"
-  xidlehook --not-when-fullscreen --timer 900 'i3lock -t -i $HOME/Pictures/wallpapers/lockImage.png' '' &
+  notify-send "setting screenlock timer to $inminuts minuts"
+  xidlehook --not-when-fullscreen --timer $locktimer 'i3lock -t -i $HOME/Pictures/wallpapers/lockImage.png' '' &
 fi
 
 python3 $HOME/DevLab/Scripts/ron_bot/main.py &
