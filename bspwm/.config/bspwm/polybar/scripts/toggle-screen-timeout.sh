7685d2361983c6647417cefeafe12abda41f40df
eyal670 <eyal670@gmail.com>
2024-04-04 11:57:55 +0300

    polybar - add screen timeout control module

---
+++ bspwm/.config/bspwm/polybar/scripts/toggle-screen-timeout.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Set up commands
+command1="xset s 0 0 && xset -dpms && pkill xidlehook"
+command2="xset s 600 600 && xset dpms && ~/.config/bspwm/bspwmrc"
+
+# Set up the state variable
+STATE=$(xset q | grep "DPMS is" | awk '{print $3}')
+
+# Run commands based on the value of STATE variable
+if [ "$STATE" = "Enabled" ]; then
+    dunstify "Disable auto display off on idle"
+    eval $command1
+elif [ "$STATE" = "Disabled" ]; then
+    dunstify "Enable auto display off on idle"
+    eval $command2
+fi
