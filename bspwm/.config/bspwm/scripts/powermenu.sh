eecdfdfa28f44f0617e73576dca3dc9451973077
eyal670 <eyal670@gmail.com>
2025-05-19 14:22:06 +0300

    powermenu - add keyboard layout fix entry

--- bspwm/.config/bspwm/scripts/powermenu.sh
+++ bspwm/.config/bspwm/scripts/powermenu.sh
@@ -1,12 +1,13 @@
 #! /bin/sh
 
-chosen=$(printf "  Power Off\n  Restart\n  Lock\n  Logout\n直 Networks" | rofi -dmenu -i -theme-str '@import "power.rasi"')
+chosen=$(printf "  Power Off\n  Restart\n  Lock\n󰍃  Logout\n󰖩  Networks\n  Fix keyboard layout" | rofi -dmenu -i -theme-str '@import "power.rasi"')
 
 case "$chosen" in
 	"  Power Off") poweroff ;;
 	"  Restart") reboot ;;
 	"  Lock") i3lock -t -i $HOME/Pictures/wallpapers/lockImage.png  ;;
-	"  Logout") kill -9 -1  ;;
-	"直 Networks") alacritty --class scratchTerm,scratchTerm -e "nmtui" ;;
+	"󰍃  Logout") kill -9 -1  ;;
+	"󰖩  Networks") alacritty --class scratchTerm,scratchTerm -e "nmtui" ;;
+	"  Fix keyboard layout") setxkbmap -option grp:alt_shift_toggle -layout us,il; setxkbmap -option caps:escape; notify-send "keyboard layout fixed" ;;
 	*) exit 1 ;;
 esac
