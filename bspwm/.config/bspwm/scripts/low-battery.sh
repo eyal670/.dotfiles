a13ea57d61c6b5c595d8a7328edd606a184b8cff
eyal670 <eyal670@gmail.com>
2025-05-22 17:42:59 +0300

    bspwm - send notification to flip-5

--- bspwm/.config/bspwm/scripts/low-battery.sh
+++ bspwm/.config/bspwm/scripts/low-battery.sh
@@ -8,15 +8,26 @@ export DBUS_SESSION_BUS_ADDRESS=unix:path=/run/user/1000/bus
 BATTERY_CAPACITY=$(cat /sys/class/power_supply/BAT0/capacity)
 BATTERY_STATUS=$(cat /sys/class/power_supply/BAT0/status)
 
-ALERT_LIMIT=15
+ALERT_LIMIT=22
 LOW_LIMIT=5
 
 if [[ $BATTERY_CAPACITY -le $ALERT_LIMIT && $BATTERY_CAPACITY -gt $LOW_LIMIT && $BATTERY_STATUS = "Discharging" ]]; then
-  notify-send "battery is low" "<b>please connect the charger NOW! </b> \ncapacity: $BATTERY_CAPACITY% | status: $BATTERY_STATUS"
+  T="battery is low"
+  M="<b> please connect the charger NOW! </b>
+capacity: $BATTERY_CAPACITY% | status: $BATTERY_STATUS"
+  notify-send "$T" "$M"
+  notify-send "sending msg to flip5"
+  curl -d "asus-arch: $M" ntfy.sh/flip5-NV5YQkJ5C8X # send notification to flip5
 fi
 
 if [[ $BATTERY_CAPACITY -le $LOW_LIMIT && $BATTERY_STATUS = "Discharging" ]]; then
-  notify-send "battery is critical low" "<b>computer will shutdown in 60 sec</b> \ncapacity: $BATTERY_CAPACITY% | status: $BATTERY_STATUS"
+  T="battery is critical low"
+  M="<b> computer will shutdown in 60 sec </b>
+capacity: $BATTERY_CAPACITY% | status: $BATTERY_STATUS"
+  notify-send "$T" "$M"
+  notify-send "sending msg to flip5"
+  curl -d "asus-arch: $M" ntfy.sh/flip5-NV5YQkJ5C8X # send notification to flip5
+
   sleep 60s
   BATTERY_STATUS=$(cat /sys/class/power_supply/BAT0/status)
   if [[ $BATTERY_STATUS = "Discharging" ]]; then
