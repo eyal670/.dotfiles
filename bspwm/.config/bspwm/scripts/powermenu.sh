bde65785872c9f76f92f3c78d1df396231a71120
eyal670 <eyal670@gmail.com>
2025-06-17 11:49:44 +0300

    rofi - powermenu - add entry to charge battery to 100%

--- bspwm/.config/bspwm/scripts/powermenu.sh
+++ bspwm/.config/bspwm/scripts/powermenu.sh
@@ -1,6 +1,6 @@
 #! /bin/sh
 
-chosen=$(printf "  Power Off\n  Restart\n  Lock\n󰍃  Logout\n󰖩  Networks\n  Fix keyboard layout" | rofi -dmenu -i -theme-str '@import "power.rasi"')
+chosen=$(printf "  Power Off\n  Restart\n  Lock\n󰍃  Logout\n󰖩  Networks\n  Fix keyboard layout\n  Charge full battery" | rofi -dmenu -i -theme-str '@import "power.rasi"')
 
 case "$chosen" in
 	"  Power Off") poweroff ;;
@@ -9,5 +9,6 @@ case "$chosen" in
 	"󰍃  Logout") kill -9 -1  ;;
 	"󰖩  Networks") alacritty --class scratchTerm,scratchTerm -e "nmtui" ;;
 	"  Fix keyboard layout") setxkbmap -option grp:alt_shift_toggle -layout us,il; setxkbmap -option caps:escape; notify-send "keyboard layout fixed" ;;
+	"  Charge full battery") alacritty --class scratchTerm,scratchTerm -e sh -c 'echo "approve chanrging battery to 100%" && sudo tlp fullcharge && notify-send "charge battery to 100%"' ;;
 	*) exit 1 ;;
 esac
