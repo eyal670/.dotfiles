cee16c8fcc926f26e2f321639b7e93bff9a2c2c1
eyal670 <eyal670@gmail.com>
2026-01-06 12:46:31 +0200

    Revert "polybar - rofi trackingtime menu - run python script using uv"
    This reverts commit a3a7260fc255c8c6248c77c9964478e8c55487ff.

--- bspwm/.config/bspwm/polybar/scripts/trackingtime-today/trackingtime-menu.sh
+++ bspwm/.config/bspwm/polybar/scripts/trackingtime-today/trackingtime-menu.sh
@@ -3,17 +3,17 @@
 lines=4
 last_month_start=$(date --date="$(date +'%Y-%m-01') - 1 month" +%Y-%m-%d)
 last_month_end=$(date --date="$(date +'%Y-%m-01') - 1 second" +%Y-%m-%d)
-chosen=$(echo "this month|last month|today|set manually" | rofi -sep '|' -dmenu -p "TrackingTime total >" -i -l $lines -theme-str 'window { anchor:northeast;location:northeast; }')
+chosen=$(echo "this month|last month|today|set manually" | rofi -sep '|' -dmenu -p "TrackingTime total >" -i -l $lines -theme-str 'window { anchor:northeast;location:northeast; }' )
 case "$chosen" in
-"today") $HOME/.local/bin/uv run $HOME/.config/bspwm/polybar/scripts/trackingtime-today/main.py ;;
-"this month") $HOME/.local/bin/uv run $HOME/.config/bspwm/polybar/scripts/trackingtime-today/main.py -tf month ;;
-"last month") $HOME/.local/bin/uv run $HOME/.config/bspwm/polybar/scripts/trackingtime-today/main.py \
-	-tf set \
-	-sd $last_month_start \
-	-ed $last_month_end ;;
-"set manually") $HOME/.local/bin/uv run $HOME/.config/bspwm/polybar/scripts/trackingtime-today/main.py \
-	-tf set \
-	-sd $(echo $last_month_start | rofi -sep '|' -dmenu -p "start date > " -mesg "format: YYYY-M-D" -l $lines -theme-str 'window { anchor:northeast;location:northeast; }') \
-	-ed $(echo $last_month_end | rofi -sep '|' -dmenu -p "end date > " -mesg "format: YYYY-M-D" -l $lines -theme-str 'window { anchor:northeast;location:northeast; }') ;;
-*) exit 1 ;;
+	"today") $HOME/.config/bspwm/polybar/scripts/trackingtime-today/main.py ;;
+	"this month") $HOME/.config/bspwm/polybar/scripts/trackingtime-today/main.py -tf month ;;
+    "last month") $HOME/.config/bspwm/polybar/scripts/trackingtime-today/main.py \
+        -tf set \
+        -sd $last_month_start \
+        -ed $last_month_end   ;;
+	"set manually") $HOME/.config/bspwm/polybar/scripts/trackingtime-today/main.py \
+        -tf set \
+        -sd $(echo $last_month_start | rofi -sep '|' -dmenu -p "start date > " -mesg "format: YYYY-M-D" -l $lines -theme-str 'window { anchor:northeast;location:northeast; }') \
+        -ed $(echo $last_month_end | rofi -sep '|' -dmenu -p "end date > " -mesg "format: YYYY-M-D" -l $lines -theme-str 'window { anchor:northeast;location:northeast; }')   ;;
+	*) exit 1 ;;
 esac
