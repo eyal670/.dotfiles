8b0c658491b5c3ae0af85c3ceff158edbd930080
eyal670 <eyal670@gmail.com>
2026-01-06 12:53:13 +0200

    polybar - trackingtime script run using "uv run" shebang

--- bspwm/.config/bspwm/polybar/scripts/trackingtime-today/main.py
+++ bspwm/.config/bspwm/polybar/scripts/trackingtime-today/main.py
@@ -1,4 +1,10 @@
-#!/usr/bin/env python3
+#!/usr/bin/env -S uv run --script
+# /// script
+# requires-python = ">=3.13"
+# dependencies = [
+#     "load-dotenv>=0.1.0",
+# ]
+# ///
 
 # Title: TrackingTime today total hrs
 # Description: get total combined hrs from multiple TrackingTime accounts
